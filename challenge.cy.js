a44d7b9918922c22bc039535ccc928ace68beae8
Sava Barbarov <45712571+savabarbarov96@users.noreply.github.com>
2025-02-12 20:21:51 +0200

    Create challenge.cy.js
    Adding the challenge file.

---
+++ challenge.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Automation Exercise Website Tests', () => {
+  const baseUrl = 'https://practice-automation.com/';
+
+  it('should navigate to the Signup/Login page', () => {
+    
+  });
+
+
+});
