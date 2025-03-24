f892bf9b788254d01294f7b530a201468d3e6bd3
Sava Barbarov <45712571+savabarbarov96@users.noreply.github.com>
2025-03-24 14:22:43 +0200

    Update challenge.cy.js

--- challenge.cy.js
+++ challenge.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+/// Navigate to https://practice-automation.com/form-fields/ 
+// Create simple test cases for the form
+// Validate if the form can be submitted without the required fields
 describe('Automation Exercise Website Tests', () => {
   const baseUrl = 'https://practice-automation.com/';
 
