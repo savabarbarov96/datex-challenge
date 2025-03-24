945c7492da2e83f07ecfb0658365e436a468f393
Sava Barbarov <45712571+savabarbarov96@users.noreply.github.com>
2025-03-24 14:25:01 +0200

    Update challenge.cy.js

--- challenge.cy.js
+++ challenge.cy.js
@@ -3,6 +3,8 @@
 /// Navigate to https://practice-automation.com/form-fields/ 
 // Create simple test cases for the form
 // Validate if the form can be submitted without the required fields
+
+// Navigate to https://practice-automation.com/spinners/ and make assertion against the spinner, once the spinner is finished write cy.log("Spinner is done! ")
 describe('Automation Exercise Website Tests', () => {
   const baseUrl = 'https://practice-automation.com/';
 
